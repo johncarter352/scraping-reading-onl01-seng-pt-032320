cf57432e3a09cc5ef781632c05adf554bfb58df0
John Carter <johncarter352@gmail.com>
2020-04-30 06:34:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,6 +2,7 @@
 require 'nokogiri'
 require 'open-uri'
  
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
 
 doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
